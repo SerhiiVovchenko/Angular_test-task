67b7a8df777f0c4691ec39d03df4dc7b8bb03bff
Serhii Vovchenko <seriivigas@gmail.com>
2023-07-01 16:15:24 +0300

    develop

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'serg'`, () => {
+  it(`should have as title 'my-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('serg');
+    expect(app.title).toEqual('my-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('serg app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-angular app is running!');
   });
 });
